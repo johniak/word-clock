f859a79d168babff68e19c008e8ca4d4aba86dbf
Jan Romaniak <jan.romaniak@tivix.com>
2024-10-06 20:50:16 +0200

    fix eight hour

--- esp/wordclock/src/ClockDisplayHAL.cpp
+++ esp/wordclock/src/ClockDisplayHAL.cpp
@@ -1,7 +1,30 @@
 #include "ClockDisplayHAL.h"
 
 ClockDisplayHAL::WordMapping const ClockDisplayHAL::WORDS_TO_LEDS[] = {
-    {"HOUR_1", 20, 22}, {"HOUR_2", 45, 47}, {"HOUR_3", 15, 19}, {"HOUR_4", 67, 70}, {"HOUR_5", 40, 43}, {"HOUR_6", 12, 14}, {"HOUR_7", 55, 59}, {"HOUR_8", 29, 35}, {"HOUR_9", 36, 39}, {"HOUR_10", 9, 11}, {"HOUR_11", 24, 29}, {"HOUR_12", 48, 53}, {"OCLOCK", 0, 5}, {"PAST", 60, 63}, {"TO", 63, 64}, {"MINUTES", 77, 83}, {"THIRTY", 84, 89}, {"TWENTY", 102, 107}, {"TWENTYFIVE", 98, 107}, {"FIVE", 98, 101}, {"TEN", 91, 93}, {"FIFTEEN", 110, 116}, {"IS", 127, 128}, {"IT", 130, 131}};
+    {"HOUR_1", 20, 22},
+    {"HOUR_2", 45, 47},
+    {"HOUR_3", 15, 19},
+    {"HOUR_4", 67, 70},
+    {"HOUR_5", 40, 43},
+    {"HOUR_6", 12, 14},
+    {"HOUR_7", 55, 59},
+    {"HOUR_8", 31, 35},
+    {"HOUR_9", 36, 39},
+    {"HOUR_10", 9, 11},
+    {"HOUR_11", 24, 29},
+    {"HOUR_12", 48, 53},
+    {"OCLOCK", 0, 5},
+    {"PAST", 60, 63},
+    {"TO", 63, 64},
+    {"MINUTES", 77, 83},
+    {"THIRTY", 84, 89},
+    {"TWENTY", 102, 107},
+    {"TWENTYFIVE", 98, 107},
+    {"FIVE", 98, 101},
+    {"TEN", 91, 93},
+    {"FIFTEEN", 110, 116},
+    {"IS", 127, 128},
+    {"IT", 130, 131}};
 
 ClockDisplayHAL::ClockDisplayHAL(uint8_t pin, uint8_t brightness)
     : pixels(NUM_LEDS, pin, NEO_GRB + NEO_KHZ800), brightness(brightness)
